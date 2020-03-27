aa3c476c7dd1093076141ad565507144bda0362e
Nicolas Plaisant <nicolasbcp@gmail.com>
2020-03-27 18:04:21 -0300

    Criar Secret Key e Token Service
    Adicionada classe Secret a qual contém um Guid que é responsável por validar os tokens da aplicação.
    
    Separado dos controllers, o service de Token foi adicionado para configurar os serviços de autentição

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "089a85cc77a4439cb36d105a9cc61595";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,12 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Shop.Data;
 
 namespace Shop
@@ -20,6 +23,24 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var key  = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             // Informa qual BD será usado
             services.AddDbContext<DataContext>(
@@ -43,6 +64,7 @@ namespace Shop
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
